9d83cf88a2162f1a76471f1de2737d980476bbee
md sufiyan <uaremdsufian@gmail.com>
2025-06-27 11:23:36 +0530

    crons

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
-
+import { ScheduleModule } from '@nestjs/schedule';
 @Module({
-  
+  imports: [
+    ScheduleModule.forRoot()
+  ],
   controllers: [UploadController],
   providers: [UploadService]
 })
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -20,14 +20,14 @@
 //     }
 // }
 
-import { Injectable } from '@nestjs/common';
+import { Injectable ,Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-
+import { Cron } from '@nestjs/schedule';
 @Injectable()
 export class UploadService {
   private readonly s3Client: S3Client;
-
+ private readonly logger = new Logger();
   constructor(private readonly configService: ConfigService) {
     this.s3Client = new S3Client({
       region: this.configService.getOrThrow('AWS_S3_REGION'),
@@ -42,5 +42,9 @@ export class UploadService {
         Body: file,
       }),
     );
+  }
+  @Cron('45 * * * * *')
+  handleCron() {
+    this.logger.debug('Called when the current second is 45');
   }
 }
